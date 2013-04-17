5893ae2b66e4e580a326768253ce31d43e488e7f
Simon Coffey <simon@tribesports.com>
2013-04-17 15:16:25 +0100

    Tests in Jasmine

--- spec/eliza/decomp_spec.js
+++ spec/eliza/decomp_spec.js
@@ -1,23 +1,25 @@
-var Test = require('../../vendor/jstest').Test,
-    Decomp = require('../../lib/eliza/decomp');
+var Decomp = require('../../lib/eliza/decomp');
 
-Test.describe("Decomp", function() { with(this) {
-  before(function() {
-    var regex   = /hello i'm (.*)/,
-        phrases = ["hello (1)", "go away (1)"];
-    this.decomp = new Decomp(regex, phrases);
+describe("Decomp", function() { with(this) {
+  var regex   = /hello i'm (.*)/,
+      phrases = ["hello (1)", "go away (1)"],
+      decomp;
+
+  beforeEach(function() {
+    decomp = new Decomp(regex, phrases);
   })
 
-  it("returns false on no match", function() { with(this) {
-    assertEqual(decomp.match("this doesn't match"), false);
-  }})
+  it("returns false on no match", function() {
+    expect(decomp.match("this doesn't match")).toEqual(false);
+  })
 
-  it("substitutes captured blocks", function() { with(this) {
-    assertEqual(decomp.match("hello i'm simon"), "hello simon");
-  }})
+  it("substitutes captured blocks", function() {
+    expect(decomp.match("hello i'm simon")).toEqual("hello simon");
+  })
 
-  it("cycles through its phrases", function() { with(this) {
-    decomp.match("hello i'm simon");
-    assertEqual(decomp.match("hello i'm edgar"), "go away edgar");
-  }})
+  it("cycles through its phrases", function() {
+    expect(decomp.match("hello i'm simon")).toEqual("hello simon");
+    expect(decomp.match("hello i'm edgar")).toEqual("go away edgar");
+    expect(decomp.match("hello i'm james")).toEqual("hello james");
+  })
 }})
