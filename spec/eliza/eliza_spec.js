249f58b1aa38b643daea6513cb096cde93a11f17
Simon Coffey <simon@tribesports.com>
2013-08-11 15:25:26 +0100

    More eliza integ. specs

--- spec/eliza/eliza_spec.js
+++ spec/eliza/eliza_spec.js
@@ -28,16 +28,45 @@ describe("Eliza", function() { with(this) {
   };
 
   beforeEach(function() {
-    this.eliza = new Eliza(script);
+    this.client = {
+      say: function(phrase) { }
+    };
+    spyOn(this.client, 'say');
+
+    this.eliza = new Eliza(this.client, script);
   })
 
   describe("start()", function() { with(this) {
     it("replies with the initial greeting", function() { with(this) {
-      expect(eliza.start()).toEqual("hello there");
+      expect(client.say).toHaveBeenCalledWith("hello there");
     }})
   }})
 
   describe("say()", function() { with(this) {
+    it("matches keywords", function() { with(this) {
+      eliza.say("I'm sorry but your a dick");
+
+      expect(client.say).toHaveBeenCalledWith("Don't apologise");
+    }})
+
+    it("prioritises higher weight keywords", function() { with(this) {
+      eliza.say("sorry, I didn't catch your name");
+
+      expect(client.say).toHaveBeenCalledWith("I don't like names");
+    }})
+
+    it("Uses a default phrase if no matches are found", function() { with(this) {
+      eliza.say("this contains no keywords");
+
+      expect(client.say).toHaveBeenCalledWith("Please go on");
+    }})
+
+    it("says goodbye if a quit phrase is detected", function() { with(this) {
+      eliza.say("quit");
+
+      expect(client.say).toHaveBeenCalledWith("Fine, go.");
+      expect(eliza.isFinished()).toEqual(true);
+    }})
   }})
 
 }})
