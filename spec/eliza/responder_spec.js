1c9484145565947a06cd2896b23a90c89870d599
Simon Coffey <simon@tribesports.com>
2013-09-05 11:48:37 +0100

    More telling, less asking

--- spec/eliza/responder_spec.js
+++ spec/eliza/responder_spec.js
@@ -4,6 +4,9 @@ describe("Responder", function() { with(this) {
   var defaults = ["Welp"];
 
   beforeEach(function() {
+    this.client = { say: function() {} };
+    spyOn(this.client, 'say');
+
     this.pre  = { };
     this.post = { };
 
@@ -22,27 +25,30 @@ describe("Responder", function() { with(this) {
 
   describe(".respond()", function() { with(this) {
     it("calls match() on each key until one matches", function() { with(this) {
-      responder.respondTo("dont do that")
+      responder.respondTo("dont do that", client)
 
       expect(key1.match).toHaveBeenCalledWith("dont do that", responder);
       expect(key2.match).toHaveBeenCalledWith("dont do that", responder);
       expect(key3.match).not.toHaveBeenCalled();
     }})
 
-    it("returns the value it is told to respond with", function() { with(this) {
-      expect(responder.respondTo("dont do that")).toEqual("hello");
+    it("says the response to the client", function() { with(this) {
+      responder.respondTo("dont do that", client);
+      expect(client.say).toHaveBeenCalledWith("hello");
     }})
 
     it("returns 'Welp' if it cannot find a response", function() { with(this) {
       key2.match = function(p,r) {};
 
-      expect(responder.respondTo("dont do that")).toEqual("Welp");
+      responder.respondTo("dont do that", client);
+
+      expect(client.say).toHaveBeenCalledWith("Welp");
     }})
 
     it("performs pre-substitutions on the input phrase", function() { with(this) {
       pre["dont"] = "don't";
 
-      responder.respondTo("dont do that");
+      responder.respondTo("dont do that", client);
 
       expect(key1.match).toHaveBeenCalledWith("don't do that", responder);
     }})
@@ -50,7 +56,9 @@ describe("Responder", function() { with(this) {
     it("performs post-substitutions on the received response", function() { with(this) {
       post["hello"] = "goodbye";
 
-      expect(responder.respondTo("dont do that")).toEqual("goodbye");
+      responder.respondTo("dont do that", client);
+
+      expect(client.say).toHaveBeenCalledWith("goodbye");
     }})
   }})
 
